9dcec8c2ce5634389cdc3551ce08e3075230e3f3
Cadie Brown <cadie.brown@gmail.com>
2019-03-14 08:17:16 -0400

    Add comment to today_get_nav_type

--- includes/nav-functions.php
+++ includes/nav-functions.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * Overrides the UCF WordPress Theme function for returning a site nav
+ * type to always return the site-specific nav (the main site nav should
+ * never be used in this theme.)
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @return string The nav type name
+ */
 function today_get_nav_type() {
 	return '';
 }
