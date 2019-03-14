fed3c9a60bf2570b948246b41250875b425a9c6c
Cadie Brown <cadie.brown@gmail.com>
2019-03-14 13:58:10 -0400

    Include utilities and weather functions

--- functions.php
+++ functions.php
@@ -4,3 +4,5 @@
 include_once 'includes/config.php';
 include_once 'includes/meta.php';
 include_once 'includes/nav-functions.php';
+include_once 'includes/utilities.php';
+include_once 'includes/weather-functions.php';
