d99311aaf8544c0e2df5692b290c5a90f69b93cd
Cadie Brown <cadie.brown@gmail.com>
2019-03-19 16:18:13 -0400

    Wrap header and content checks for certain post types in WP_Post check

--- includes/header-functions.php
+++ includes/header-functions.php
@@ -13,17 +13,15 @@
  * @return string The determined header type
  */
 function today_get_header_type( $header_type, $obj ) {
-	$post_type     = $obj->post_type;
-	$post_template = get_page_template_slug( $obj->ID );
+	if ( $obj instanceof WP_Post ) {
+		$post_type     = $obj->post_type;
+		$post_template = get_page_template_slug( $obj->ID );
 
-	if ( $obj instanceof WP_Post && $post_type === 'post' ) {
-		$header_type = 'post';
-	} elseif (
-		$obj instanceof WP_Post &&
-		$post_type === 'page' &&
-		$post_template === 'template-section.php'
-	) {
-		$header_type = 'section';
+		if ( $post_type === 'post' ) {
+			$header_type = 'post';
+		} elseif ( $post_type === 'page' && $post_template === 'template-section.php' ) {
+			$header_type = 'section';
+		}
 	}
 
 	return $header_type;
@@ -42,17 +40,15 @@ add_filter( 'ucfwp_get_header_type', 'today_get_header_type', 11, 2 );
  * @return string The determined header content type
  */
 function today_get_header_content_type( $content_type, $obj ) {
-	$post_type     = $obj->post_type;
-	$post_template = get_page_template_slug( $obj->ID );
+	if ( $obj instanceof WP_Post ) {
+		$post_type     = $obj->post_type;
+		$post_template = get_page_template_slug( $obj->ID );
 
-	if ( $obj instanceof WP_Post && $post_type === 'post' ) {
-		$content_type = 'post';
-	} elseif (
-		$obj instanceof WP_Post &&
-		$post_type === 'page' &&
-		$post_template === 'template-section.php'
-	) {
-		$content_type = 'section';
+		if ( $post_type === 'post' ) {
+			$content_type = 'post';
+		} elseif ( $post_type === 'page' &&	$post_template === 'template-section.php' ) {
+			$content_type = 'section';
+		}
 	}
 
 	return $content_type;
