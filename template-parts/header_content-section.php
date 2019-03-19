dcdcc828f66b62c5059f56b97f98d947d11cddf3
Cadie Brown <cadie.brown@gmail.com>
2019-03-19 15:14:39 -0400

    Add custom header template parts for section pages

---
+++ template-parts/header_content-section.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Header content template for the 'Section' page template
+ */
+?>
+
+<?php
+global $post;
+
+$title = wptexturize( $post->post_title );
+?>
+
+<?php if ( $title ): ?>
+<div class="container mt-4 mt-md-5">
+	<h1 class="mb-3">
+		<?php echo $title; ?>
+	</h1>
+	<hr>
+</div>
+<?php endif; ?>
