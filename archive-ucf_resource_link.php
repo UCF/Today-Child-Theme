53b2e0229111840592c44f97c4c920ac9261fadc
Jo Dickson <jogreybill@gmail.com>
2019-03-29 16:34:23 -0400

    Updated layout for resource link archive post list; updated 'no results found' message

--- archive-ucf_resource_link.php
+++ archive-ucf_resource_link.php
@@ -1,25 +1,36 @@
 <?php get_header(); ?>
 
-<div class="container mb-5">
+<div class="container mt-3 mt-md-4 mb-5 pb-sm-4">
 	<div class="row">
 		<div class="col-lg-8">
 			<?php if ( have_posts() ) : ?>
-				<?php echo today_post_list_display_feature( '', $posts, array( 'posts_per_row' => 3, 'layout' => 'vertical' ) ); ?>
+				<?php while ( have_posts() ): the_post(); ?>
+					<div class="pb-2">
+						<?php
+						echo today_display_feature_horizontal( $post, array(
+							'layout'        => 'horizontal',
+							'show_image'    => false,
+							'show_excerpt'  => true,
+							'show_subhead'  => true
+						) );
+						?>
+					</div>
+				<?php endwhile; ?>
 				<?php ucfwp_the_posts_pagination(); ?>
 			<?php else : ?>
 				<div class="alert alert-info">
-					<p>There are currently no stories for <?php echo date( 'F Y' ); ?>.</p>
+					<p>No recent news articles found.</p>
 				</div>
 			<?php endif; ?>
 		</div>
 		<div class="col-lg-4 pl-lg-5 mt-4 mt-lg-0">
-		<h2 class="h6 text-uppercase text-default-aw mb-4">Resources</h2>
-		<?php
-		wp_nav_menu( array(
-			'menu'       => 'Resources',
-			'menu_class' => 'list-unstyled'
-		) );
-		?>
+			<h2 class="h6 text-uppercase text-default-aw mb-4">Resources</h2>
+			<?php
+			wp_nav_menu( array(
+				'menu'       => 'Resources',
+				'menu_class' => 'list-unstyled'
+			) );
+			?>
 		</div>
 	</div>
 </div>
