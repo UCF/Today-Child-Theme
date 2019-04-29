c7b988ba74bcf5a5e755a2b2c1ef96e28432806d
RJ Bruneel <rj.bruneel@ucf.edu>
2019-04-29 15:01:57 -0400

    added template specific logic

--- template-parts/header_content-post.php
+++ template-parts/header_content-post.php
@@ -12,12 +12,15 @@ $primary_category = ( $primary_category ) ? wptexturize( $primary_category->name
 $title            = wptexturize( $post->post_title );
 $deck             = get_field( 'post_header_deck', $post );
 $meta             = today_get_post_meta_info( $post );
+$post_template    = get_page_template_slug( $post->ID );
 ?>
 
 <?php if ( $title ): ?>
 <div class="container mt-4 mt-md-5">
+	<?php if ( $post_template === 'single.php' ) : ?>
 	<div class="row">
 		<div class="col-xl-10 offset-xl-1">
+	<?php endif; ?>
 			<?php if ( $primary_category ): ?>
 			<span class="d-block mb-3 text-default-aw font-weight-bold text-uppercase"><?php echo $primary_category; ?></span>
 			<?php endif; ?>
@@ -37,7 +40,9 @@ $meta             = today_get_post_meta_info( $post );
 				<?php echo $meta; ?>
 			</div>
 			<?php endif; ?>
+	<?php if ( $post_template === 'single.php' ) : ?>
 		</div>
 	</div>
+	<?php endif; ?>
 </div>
 <?php endif; ?>
