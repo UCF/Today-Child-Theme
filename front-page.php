1d83a9649fedc615ac64f4b3d48901d4cc3e1093
Jo Dickson <jogreybill@gmail.com>
2019-05-17 16:33:12 -0400

    Added resources menu above footer on homepage

--- front-page.php
+++ front-page.php
@@ -1,27 +1,38 @@
 <?php get_header(); the_post(); ?>
 
 <?php
-$primary = today_get_homepage_content( $post->ID, true );
+$primary   = today_get_homepage_content( $post->ID, true );
 $secondary = today_get_homepage_content( $post->ID );
+$resources = trim( wp_nav_menu( array(
+	'menu'       => 'Resources',
+	'menu_class' => 'list-unstyled d-flex flex-column flex-lg-row align-items-center justify-content-lg-between',
+	'echo'       => false
+) ) );
 ?>
 
-<article class="<?php echo $post->post_status; ?> post-list-item">
-	<div class="container mt-4 mt-md-5 mb-5 pb-sm-4">
-		<?php echo $primary; ?>
-		<?php if ( get_field( 'enable_sidebar' ) ) : ?>
-		<div class="row">
-			<div class="col-lg-8">
-				<?php echo $secondary; ?>
-			</div>
-			<div class="col-lg-4 pl-lg-5 mt-5 mt-lg-0">
-				<?php echo today_get_sidebar_markup( $post->ID ); ?>
-			</div>
-		</div>
-		<?php else : ?>
-			<?php echo $secondary; ?>
-		<?php endif; ?>
+<div class="container mt-4 mt-md-5 mb-5 pb-sm-4">
 
+	<?php echo $primary; ?>
+	<?php if ( get_field( 'enable_sidebar' ) ) : ?>
+	<div class="row">
+		<div class="col-lg-8">
+			<?php echo $secondary; ?>
+		</div>
+		<div class="col-lg-4 pl-lg-5 mt-5 mt-lg-0">
+			<?php echo today_get_sidebar_markup( $post->ID ); ?>
+		</div>
 	</div>
-</article>
+	<?php else : ?>
+		<?php echo $secondary; ?>
+	<?php endif; ?>
+
+</div>
+
+<?php if ( $resources ): ?>
+<div class="container mb-4 text-center">
+	<hr class="my-4">
+	<?php echo $resources; ?>
+</div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
