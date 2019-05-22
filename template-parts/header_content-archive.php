555504386c17cf7f73fa5d35517e357f18a9d866
Cadie Brown <cadie.brown@gmail.com>
2019-05-22 17:24:42 -0400

    Remove hrs across the site and increase spacing

--- template-parts/header_content-archive.php
+++ template-parts/header_content-archive.php
@@ -11,11 +11,11 @@ $header_content = '';
 
 if ( is_date() ) {
 	$title = today_archive_pagination( '<h1 class="text-uppercase h4 mb-0">' . $title_string . '</h1>' );
-	$header_content = '<div class="container mt-3">' . $title . '<hr></div>';
+	$header_content = '<div class="container mt-3">' . $title . '</div>';
 }
 else {
 	$title = '<h1 class="mb-3">' . $title_string . '</h1>';
-	$header_content = '<div class="container mt-4 mt-md-5">' . $title . '<hr></div>';
+	$header_content = '<div class="container mt-4 mt-md-5">' . $title . '</div>';
 }
 ?>
 
