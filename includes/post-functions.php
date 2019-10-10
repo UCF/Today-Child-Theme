cdd9942a4b6a08a09720c6ae41254fd0829c0bdd
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-10-10 09:35:29 -0400

    Added logic to add tags to the GTM data layer

--- includes/post-functions.php
+++ includes/post-functions.php
@@ -384,3 +384,24 @@ function today_get_post_tag_headlines( $post, $headlines=array() ) {
 	endif;
 	return ob_get_clean();
 }
+
+function today_add_tags_to_data_layer() {
+	// If this isn't a single post, return.
+	if ( ! is_singular( 'post' ) ) return;
+
+	global $post;
+
+	$gtm_id = get_theme_mod( 'gtm_id' );
+	if ( $gtm_id ) :
+		$terms = wp_get_post_terms( $post->ID, 'post_tag', array( 'fields' => 'names') );
+?>
+<script>
+		dataLayer.push({
+			'tags': <?php echo json_encode( $terms ); ?>
+		});
+</script>
+<?php
+	endif;
+}
+
+add_action( 'wp_head', 'today_add_tags_to_data_layer', 3 );
