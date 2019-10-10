6ad2f163889de2c59cfb09b7e961e4141b265d9a
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-10-10 09:59:22 -0400

    Moved tags back to earlier execution

--- includes/post-functions.php
+++ includes/post-functions.php
@@ -396,18 +396,15 @@ function today_add_tags_to_data_layer() {
 		$terms = wp_get_post_terms( $post->ID, 'post_tag', array( 'fields' => 'names') );
 ?>
 <script>
-
-document.addEventListener('load', function() {
-	<?php foreach( $terms as $term ) : ?>
-	dataLayer.push({
-		'event': 'tagPushed'
-		'tag': '<?php echo json_encode( $term ); ?>'
-	});
-		<?php endforeach; ?>
+<?php foreach( $terms as $term ) : ?>
+dataLayer.push({
+	'event': 'tagPushed'
+	'tag': '<?php echo json_encode( $term ); ?>'
 });
+<?php endforeach; ?>
 </script>
 <?php
 	endif;
 }
 
-add_action( 'wp_head', 'today_add_tags_to_data_layer', 10 );
+add_action( 'wp_head', 'today_add_tags_to_data_layer', 3 );
