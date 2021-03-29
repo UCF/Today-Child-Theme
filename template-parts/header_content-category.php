9c4ba46532386b677b01144f46cb14e684b129bb
Cadie Stockman <cadie.brown@gmail.com>
2021-03-29 14:54:40 -0400

    Add back in title check

--- template-parts/header_content-category.php
+++ template-parts/header_content-category.php
@@ -10,8 +10,10 @@ $term = get_queried_object();
 $title = get_field( 'category_page_headline', $term ) ?: ucfwp_get_header_title( null ) . ' News';
 ?>
 
+<?php if ( $title ): ?>
 <div class="container mt-4 mt-md-5">
 	<h1 class="mb-4">
 		<?php echo $title; ?>
 	</h1>
 </div>
+<?php endif; ?>
